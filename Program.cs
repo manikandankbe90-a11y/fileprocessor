fb4d6e8620ea6cf3d00d45dbb3e8e4e1bd113632
manikandankbe90-a11y <manikandankbe90@gmail.com>
2026-01-05 12:06:48 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,16 +1,1 @@
-// Pattern: policy:PolicyVersion attribute (e.g., policy:PolicyVersion="1.0")
-var policyVersionPattern = @"(policy:PolicyVersion\s*=\s*"")([^""]*?)("")";
-var policyVersionRegex = new Regex(policyVersionPattern, RegexOptions.IgnoreCase);
-
-if (policyVersionRegex.IsMatch(content))
-{
-    var oldVersion = policyVersionRegex.Match(content).Groups[2].Value;
-    content = policyVersionRegex.Replace(content, $"$1{newVersion}$3");
-    File.WriteAllText(file, content, Encoding.UTF8);
-    xmlFilesModified++;
-    Console.WriteLine($"  [OK] {Path.GetFileName(file)} - {oldVersion} -> {newVersion}");
-}
-else
-{
-    Console.WriteLine($"  [--] {Path.GetFileName(file)} - No policy:PolicyVersion found");
-}
+content = policyVersionRegex.Replace(content, "${1}" + newVersion + "${3}");
