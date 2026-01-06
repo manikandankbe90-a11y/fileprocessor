6d03bb81ae946f993b08eaa6c87c64e8f9e22529
manikandankbe90-a11y <manikandankbe90@gmail.com>
2026-01-07 05:15:20 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,3 @@
- var guidPattern = @"(const\s+string\s+\w+\s*=\s*"")([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12})("")";
- var guidRegex = new Regex(guidPattern);
-
- if (guidRegex.IsMatch(content))
- {
-     var oldGuid = guidRegex.Match(content).Groups[2].Value;
-     content = guidRegex.Replace(content, $"$1{newGuid}$3");
-     modified = true;
-     Console.WriteLine($"  [OK] {Path.GetFileName(file)} - GUID updated: {oldGuid} -> {newGuid}");
- }
+var versionVarName = newVersion.Replace(".", "");
+var newVersionConst = $"private const string Version{versionVarName} = \"{newVersion}\";";
+// Creates: Version316 = "3.16"  ← Valid variable name!
