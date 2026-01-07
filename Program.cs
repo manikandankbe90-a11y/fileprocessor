657294a30c66da9cdf85e1cedb3159b7a360fe82
manikandankbe90-a11y <manikandankbe90@gmail.com>
2026-01-07 07:21:50 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
-// Get the last quoted number '5'
-var currentVersionMatch = Regex.Match(content, @"'(\d+)'\s*\)");
+var guidPattern = @"(const\s+string\s+\w+\s*=\s*"")([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12})("")";
+var guidRegex = new Regex(guidPattern);
 
-// Update '5' -> '6'
-var lastValuePattern = @"'(\d+)'(\s*\))";
-content = Regex.Replace(content, lastValuePattern, $"'{currentVersion + 1}'$2");
+var guidMatches = guidRegex.Matches(content);
+foreach (Match gm in guidMatches)
+{
+    var oldGuid = gm.Groups[2].Value;
+    var oldFullMatch = gm.Value;
+    var newFullMatch = gm.Groups[1].Value + newGuid + gm.Groups[3].Value;
+    content = content.Replace(oldFullMatch, newFullMatch);
+    modified = true;
+    Console.WriteLine($"  [OK] {Path.GetFileName(file)} - GUID updated: {oldGuid} -> {newGuid}");
+}
